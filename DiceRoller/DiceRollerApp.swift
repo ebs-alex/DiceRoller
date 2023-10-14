121328efa853f02a285c598a91373003b6b19dbe
Alex <ebs.alex@icloud.com>
2023-10-14 16:06:36 -0700

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Alex Moran on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
